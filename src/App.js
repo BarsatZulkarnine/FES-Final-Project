d875407b5cf02cc58f7ef26ec0bb900f1946d907
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-03-09 22:10:30 +1100

    placehodler

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route />
         </Routes>
       </Router>
     </>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,8 +6,8 @@ function SearchBar() {
       <div className="row">
         <form id="contact__form">
           <div class="form__item">
-            <label class="form__item--label">Name</label>
-            <input class="input" name="user_name" type="text" required></input>
+            {/* <label class="form__item--label">Name</label> */}
+            <input class="input" name="user_name" type="text" placeholder="search for a moive" required></input>
           </div>
           <button id="contact__submit" class="form__submit">
             Search
