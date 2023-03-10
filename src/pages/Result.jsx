fb53714dbe2b8098bca1c01ae5c6c65f0d2fa7a5
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-03-10 21:00:22 +1100

    result page

---
+++ src/pages/Result.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../components/SearchBar";
+import Nav from "./components/nav";
+function Result() {
+  return (
+    <div>
+      <Nav />
+      <SearchBar />
+    </div>
+  );
+}
+
+export default Result;
